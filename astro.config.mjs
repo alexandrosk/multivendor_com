390ffe2c30c743af3450438ccaddfe856c08d2da
alex.koukis <alexandroskoukis@gmail.com>
2023-09-04 01:16:01 +0300

    website done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-
-
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from "@astrojs/sitemap";
-// https://astro.build/config
+
 export default defineConfig({
-   site: 'https://unwraped.design',
-  integrations: [tailwind(),  sitemap()]
+    site: 'https://unwraped.design',
+    integrations: [tailwind(),  sitemap()],
+    output: 'server',
+    adapter: vercel({
+        analytics: true,
+    }),
 });
